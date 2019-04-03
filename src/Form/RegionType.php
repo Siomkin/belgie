7fa8fbcc4d912d3f9c80cdc39cd268e270bedc6f
Siomkin Alexander <siomkin.alexander@gmail.com>
2019-04-03 21:07:14 +0300

    Region Controller

--- src/Form/RegionType.php
+++ src/Form/RegionType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Region;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,20 +21,12 @@ class RegionType extends AbstractType
             );
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
             [
-                'data_class' => 'App\Entity\Region',
+                'data_class' => Region::class,
             ]
         );
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'App_region';
     }
 }
